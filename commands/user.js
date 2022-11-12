4ac5fba8b70ed79bee4ee4c17764ab118f809737
Crazypersonalph <93847055+Crazypersonalph@users.noreply.github.com>
2022-11-12 13:48:00 +0800

    danger

--- commands/user.js
+++ commands/user.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('user')
 		.setDescription('Info about a user'),
 	async execute(interaction) {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}\nYour roles: ${interaction.user.roles}`);
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}\nYour roles: ${interaction.user.email}`);
 	},
 };
