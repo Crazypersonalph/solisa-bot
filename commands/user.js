b2fb5f63baf4b324851d46f12eb9d3023bfbfbf2
Crazypersonalph <93847055+Crazypersonalph@users.noreply.github.com>
2022-11-12 14:18:37 +0800

    idk

--- commands/user.js
+++ commands/user.js
@@ -5,7 +5,6 @@ module.exports = {
 		.setName('user')
 		.setDescription('Info about a user'),
 	async execute(interaction) {
-		const user = interaction.options.getUser('user');
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}\nYour roles: ${interaction.guild.member.roles(user)}`);
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}\nYour roles: ${interaction.guild.roles}`);
 	},
 };
