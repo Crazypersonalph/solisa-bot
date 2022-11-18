89ef84104799340c44bb2008e506308e7f3e9fd6
Crazypersonalph <93847055+Crazypersonalph@users.noreply.github.com>
2022-11-18 22:32:03 +0800

    fully revert currency

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,9 @@
 const { Events } = require('discord.js');
-const { Users } = require('../dbObjects.js');
-const { currency } = require('../index.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		const storedBalances = Users.findAll();
-		storedBalances.forEach(b => currency.set(b.user_id, b));
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
