beda722d802cb7799cb8fe6f6b1dd71b7ce93923
Crazypersonalph <93847055+Crazypersonalph@users.noreply.github.com>
2023-03-15 20:51:43 +0800

    voice

---
+++ commands/play.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('play')
+		.setDescription('Play music')
+		.addStringOption(option =>
+			option.setName('url')
+				.setDescription('The song you want to play')
+				.setRequired(true)),
+	async execute(interaction) {
+		const user = interaction.options.getUser('user');
+		let url;
+		let voiceChannel;
+		url = interaction.options.getString('url');
+
+	},
+};
