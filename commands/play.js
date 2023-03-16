7f2ceb571af5db493af23d4807ee4bd07395cd67
Crazypersonalph <93847055+Crazypersonalph@users.noreply.github.com>
2023-03-16 17:56:47 +0800

    switched over to ytdl

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
-const { spawn } = require('child_process');
-const ffmpeg = require('ffmpeg');
+const ytdl = require('ytdl-core');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -17,18 +16,13 @@ module.exports = {
 		const member = interaction.guild.members.cache.get(user);
 		console.log(member.voice);
 		console.log(member.voice.channel);
-		spawn('./yt-dlp', ['--get-url', url]).stdout.on('data', (data) => {
-			url = data;
-		});
 		const player = createAudioPlayer();
 		const connection = joinVoiceChannel({
 			channelId: member.voice.channel.id,
 			guildId: member.voice.channel.guildId,
 			adapterCreator: member.voice.guild.voiceAdapterCreator,
 		});
-		const process = new ffmpeg(url);
-		process.fnExtractSoundToMP3('music.mp3');
-		const resource = createAudioResource('music.mp3');
+		const resource = createAudioResource(ytdl(url));
 		player.play(resource);
 		connection.subscribe(player);
 		player.stop();
