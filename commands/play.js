15d04dd81cc43bfe562d60ca08473e1adec9193e
Crazypersonalph <93847055+Crazypersonalph@users.noreply.github.com>
2023-03-17 20:32:53 +0800

    put it at the top

--- commands/play.js
+++ commands/play.js
@@ -12,11 +12,11 @@ module.exports = {
 				.setDescription('The song you want to play')
 				.setRequired(true)),
 	async execute(interaction) {
+		const url = interaction.options.getString('url');
 		const stream = await ytdl(url, { filter: 'audioonly' });
 		const song_info = await ytdl.getInfo(url);
 		await interaction.reply(`Now playing: ${song_info.videoDetails.title}`);
 		const user = interaction.user.id;
-		const url = interaction.options.getString('url');
 		const member = interaction.guild.members.cache.get(user);
 		const connection = joinVoiceChannel({
 			channelId: member.voice.channel.id,
